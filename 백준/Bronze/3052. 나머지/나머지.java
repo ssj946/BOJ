f5cbe3fbe415a669c94c4f4bde63525e6d7a78b5
ssj946 <58605215+ssj946@users.noreply.github.com>
2022-06-16 16:07:33 +0900

    [Bronze II] Title: 나머지, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+				
+		int[] arr = new int[10];
+		int cnt = 1;
+		
+		for(int i=0; i<10; i++) {
+			arr[i] = sc.nextInt()%42;			
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<9; i++) {
+			if(arr[i]!=arr[i+1]) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);		
+
+	}
+
+}
